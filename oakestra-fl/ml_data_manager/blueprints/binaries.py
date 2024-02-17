dadca02adbd78be4f5d7c62b27e56450e186c5ec
Malyuk-A <malyuk.alexander1999@gmail.com>
2024-02-17 19:58:42 +0100

    Use docker-compose for data manager

--- oakestra-fl/ml_data_manager/blueprints/binaries.py
+++ oakestra-fl/ml_data_manager/blueprints/binaries.py
@@ -1,5 +1,11 @@
+import pathlib
+from datetime import datetime
+
 import flask
 import flask_openapi3
+
+CONTENT_TYPE = "application/octet-stream"
+ML_DATA_VOLUME = pathlib.Path("ml_data_volume")
 
 binaries_blp = flask_openapi3.APIBlueprint(
     "binaries",
@@ -10,18 +16,16 @@ binaries_blp = flask_openapi3.APIBlueprint(
 
 @binaries_blp.post("/")
 def post_binary_data():
-    # Check if the request has the required content type
-    print(flask.request.headers.get("Content-Type"))
-    if flask.request.headers.get("Content-Type") != "application/octet-stream":
-        return {
-            "error": f"Bad request, content type = '{flask.request.headers.get('Content-Type')}'"
-        }, 400
+
+    if flask.request.headers.get("Content-Type") != CONTENT_TYPE:
+        return {"error": f"Bad request, the Header Content-Type should be '{CONTENT_TYPE}'"}, 400
 
     # Get the binary data from the request
     binary_data = flask.request.data
 
     # Write the binary data to a file
-    with open("binary_data.bin", "wb") as file:
+    current_time = datetime.now().strftime("%Y_%m_%d_%H_%M_%S_%f")
+    with open(f"{ML_DATA_VOLUME / current_time}.bin", "wb") as file:
         file.write(binary_data)
 
     return {"message": "Data posted successfully"}, 200
