59d7302051ab4ae9fa1a38b0dbb0f2752cd53310
Malyuk-A <malyuk.alexander1999@gmail.com>
2024-05-19 17:33:14 +0200

    Polishing the DMS

--- go_node_engine/extensions/flops/FLOps.go
+++ go_node_engine/extensions/flops/FLOps.go
@@ -7,8 +7,8 @@ import (
 )
 
 func HandleFLOpsDataManager() {
-	data_manager_image := "ghcr.io/malyuk-a/flops-data-manager:latest"
-	container_name := "flops_data_manager"
+	data_manager_sidecar_image := "ghcr.io/malyuk-a/flops-data-manager-sidecar:latest"
+	container_name := "flops_data_manager_sidecar"
 
 	cmd := exec.Command("docker", "ps", "-a", "--format", "{{.Names}}")
     output, err := cmd.Output()
@@ -28,13 +28,13 @@ func HandleFLOpsDataManager() {
     }
 
 	if !containerExists {
-		cmd := exec.Command("docker", "pull", data_manager_image)
+		cmd := exec.Command("docker", "pull", data_manager_sidecar_image)
 		err := cmd.Run()
 		if err!= nil {
 			logger.ErrorLogger().Fatalf("Error pulling FLOps Data Manager image: %v\n", err)
 			return
 		}
-		cmd = exec.Command("docker", "run", "--rm", "-d", "-p", "11027:11027", "-v", "flops_data_manager_sidecar_volume:/flops_data_manager_sidecar_volume" ,"--name=flops_data_manager", data_manager_image)
+		cmd = exec.Command("docker", "run", "--rm", "-d", "-p", "11027:11027", "-v", "flops_data_manager_sidecar_volume:/flops_data_manager_sidecar_volume" ,"--name=flops_data_manager_sidecar", data_manager_sidecar_image)
 		err = cmd.Run()
 		if err!= nil {
 			logger.ErrorLogger().Fatalf("Error running container: %v\n", err)
@@ -44,30 +44,6 @@ func HandleFLOpsDataManager() {
     } else {
         logger.InfoLogger().Printf("Container %q already exists.", container_name)
     }
-
-	
-
-	// version: "3.3"
-
-	// services:
-	
-	//   ml_data_manager:
-	// 	image: ml_data_manager
-	// 	build: .
-	// 	hostname: ml_data_manager
-	// 	container_name: ml_data_manager
-	// 	expose:
-	// 	  - "11027"
-	// 	ports:
-	// 	  - "11027:11027"
-	// 	volumes:
-	// 	  - ml_data_volume:/ml_data_volume
-		  
-	// volumes:
-	//   ml_data_volume:
-	
-
-	
 
 	logger.InfoLogger().Printf("FLOps Data Manager container started successfully.")
 }
